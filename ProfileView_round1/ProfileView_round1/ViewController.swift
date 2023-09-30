8d24d01853882692bc562a5146fa40eccc63c790
Kim <cestbonciel88@knou.ac.kr>
2023-09-30 15:55:44 +0900

    Add: chapter 13. challenge1 - profile view

---
+++ ProfileView_round1/ProfileView_round1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileView_round1
+//
+//  Created by Seohyun Kim on 2023/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
